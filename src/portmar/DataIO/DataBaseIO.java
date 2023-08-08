57a39944f1365ff0fc4683a50c52201f98c5b2e0
İlhan Güler <79455914+ilhanguler@users.noreply.github.com>
2023-08-08 16:40:07 +0300

    -

--- src/portmar/DataIO/DataBaseIO.java
+++ src/portmar/DataIO/DataBaseIO.java
@@ -1,14 +1,48 @@
-
 package portmar.DataIO;
 
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DataBaseIO {
-    public void fetchTable(String query){}
-    public void fetchData(String query){}
-    public void fetchRows(String query){}
-    
-    public void createTable(String query){}
-    public void deleteTable(String query){}
-    public void insertRows(String query){}
-    public void setRows(String query){}
-    public void deleteRows(String query){}
+
+    static final String DB_URL = "jdbc:derby:db\\test";
+    static final String USER = "";
+    static final String PASS = "";
+    public String query = "";
+
+    public Statement connect() {
+        Statement st = null;
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            Statement stmt = conn.createStatement();
+            st = stmt;
+        } catch (SQLException ex) {
+        }
+        return st;
+    }
+
+    public void fetchTable(String query) {
+    }
+
+    public void fetchData(String query) {
+    }
+
+    public void fetchRows(String query) {
+    }
+
+    public void createTable(String query) {
+    }
+
+    public void deleteTable(String query) {
+    }
+
+    public void insertRows(String query) {
+    }
+
+    public void updateRows(String query) {
+    }
+
+    public void deleteRows(String query) {
+    }
 }
